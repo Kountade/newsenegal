0ad577350a5691385c73d420ff6b21ac17c855cd
Bounama Kounta <bounamakountagoudiaby@gmail.com>
2023-12-05 20:03:01 +0000

    first commit 4

--- tech/urls.py
+++ tech/urls.py
@@ -1,13 +1,10 @@
 from django.urls import path
 from tech import views
+
 urlpatterns = [
-    path("", views.home, name="home"),
-    path("blogs", views.blogs, name="blogs"),
-    path("services", views.service, name="services"),
-    path("pricing", views.pricing, name="pricing"),
-    path("contact", views.contact, name="contact"),
-    path("blogs/blogdetail/<str:title>", views.blogdetail, name="blogdetail"),
-    path("register", views.register, name="register"),
+    path('', views.home, name='home-page'),
+    path('new', views.news, name='new-page'),
+    path('politique', views.politique, name='politique-page'),
 
 
 ]
