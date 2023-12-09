4e6f8484ed39f09a9f3cdafb7f32c09d14fe5b9d
Bounama Kounta <bounamakountagoudiaby@gmail.com>
2023-12-09 17:04:43 +0000

    news detail home

--- tech/urls.py
+++ tech/urls.py
@@ -1,14 +1,21 @@
 from django.urls import path
 from tech import views
+from django.conf.urls import handler404
+
+from tech.views import custom_404
+
+handler404 = custom_404
+
 
 urlpatterns = [
     path('', views.home, name='home-page'),
-    path('new', views.news, name='new-page'),
     path('politiques', views.politique, name='politique-page'),
     path('sports', views.sports, name='sports-page'),
     path('bussness', views.business, name='bussness-page'),
     path('blogs', views.blogsene, name='blogs-page'),
     # path('newsdetail/<str:title>', views.newsdetail, name="newsdetail-page"),
+    path('home/<path:news_title>/',
+         views.home_detail, name='home_detail'),
     path('politique/<path:news_title>/',
          views.politique_detail, name='politique_detail'),
     path('sport/<path:news_title>/',
--- tech/views.py
+++ tech/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 
 # Create your views here.
@@ -5,34 +6,6 @@ import requests  # Importez requests pour effectuer des requêtes HTTP
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
-
-
-def news(request):
- #   try:
-    url = f'https://newsdata.io/api/1/news?country=sn&category=politics&apikey=pub_33467774e35656a9e0c14a93978036b649c82'
-    response = requests.get(url)
-    # Raise an exception for bad responses (4xx and 5xx status codes)
-    response.raise_for_status()
-    data = response.json()
-
-    results = data["results"]
-    # Add this line to inspect the structure of the data dictionary
-
-    # Adjust the code based on the actual structure of the API response
-    # Use get to provide a default empty list if 'articles' key is not present
-   # articles = data.get('articles', [])
-
-    context = {
-        'results': results
-    }
-    return render(request, 'pages/news.html', context)
-
-  #  except requests.exceptions.RequestException as e:
-    # Handle request errors (e.g., network issues, API errors)
-  #  context = {
-#        'error_message': f"Error fetching news: {e}"
-  #   :#   }
- #   return render(request, 'pages/error.html', context)
 
 
 def home(request):
@@ -47,9 +20,54 @@ def home(request):
     context = {
         'results': results,
         'newsblog': newsblog,
+
     }
 
     return render(request, 'pages/index.html', context)
+
+
+def home_detail(request, news_title):
+    url = f'https://newsdata.io/api/1/news?country=sn&apikey=pub_33467774e35656a9e0c14a93978036b649c82'
+    response = requests.get(url)
+    # Raise an exception for bad responses (4xx and 5xx status codes)
+    response.raise_for_status()
+    data = response.json()
+    newsblog = blog.objects.all().order_by('-date')[:4]
+    results = data["results"]
+
+    # Trouver l'article spécifique par titre
+    specific_news = next(
+        (news for news in results if news["title"] == news_title), None)
+
+    context = {
+        'specific_news': specific_news
+    }
+
+    return render(request, 'pages/index_detail.html', context)
+
+  #  except requests.exceptions.RequestException as e:
+    # Handle request errors (e.g., network issues, API errors)
+  #  context = {
+#        'error_message': f"Error fetching news: {e}"
+  #   :#   }
+ #   return render(request, 'pages/error.html', context)
+
+
+# def home(request):
+  #  url = f'https://newsdata.io/api/1/news?country=sn&apikey=pub_33467774e35656a9e0c14a93978036b649c82'
+  #  response = requests.get(url)
+    # Raise an exception for bad responses (4xx and 5xx status codes)
+   # response.raise_for_status()
+ #   data = response.json()
+   # newsblog = blog.objects.all().order_by('-date')[:4]
+  #  results = data["results"]
+
+   # context = {
+    #    'results': results,
+    #    'newsblog': newsblog,
+  #  }
+
+  #  return render(request, 'pages/index.html', context)
 
 
 def sports(request):
@@ -174,3 +192,15 @@ def newsdetail(request, title: str):
 # views.py
 
 # newsapp/views.py
+
+
+def custom_404(request, exception):
+
+    return render(request, '404.html', status=404)
+
+
+def my_view(request):
+    # ... votre code ...
+
+    # Déclencher manuellement une erreur 404
+    raise Http404("Page non trouvée")
